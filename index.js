47a1a0a82ca54b66437d7c4295fd0bf56bd0fd62
Tom MacLean <tom.j.maclean@gmail.com>
2019-03-02 21:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+var i = 0
+var ticketNum = []
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  i++;
+  ticketNum.push(i);
+  return(`Welcome, ${name}. You are ticket number ${i}`);
 }
 
 function nowServing(katzDeliLine) {
-  let i = 0;
-  while (i < katzDeliLine.length) {
-    i++;
-  }
     if (katzDeliLine.length === 0) {
       return "There is nobody waiting to be served!";
     } else 
